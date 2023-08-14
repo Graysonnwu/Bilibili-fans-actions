a3e17cb6d27ec77d8713fcf7e5b46e4f7fc72824
Graysonnwu <185429663@qq.com>
2023-08-15 02:10:09 +0800

    绘图部分修改

--- README.md
+++ README.md
@@ -1,33 +1,16 @@
-# Bilibili-fans-actions（仅保留基础模式）
+# Bilibili-fans-actions
 用 Github Actions 更新 bilibili 粉丝数
 
 ![本人粉丝数变化](img/22245854_diff_follower_ori.png?raw=true)
 本人近期粉丝数变化图：（透明底白字，黑暗模式适用）
 
 
-代码改编自：https://github.com/guodongxiaren/py （改编得很水，期待大佬完善）
+代码改编自：https://github.com/guodongxiaren/py
 
 ---
 ## 说明
-- **【基础】只更新粉丝数，无需SESSDATA：**
+- **更新粉丝数**
   > `bilibili.py` + `bilibili.sh`  + `uid.txt` --> `./data` 
 
-- **【画图】粉丝数变化图**
+- **画图**
   > `draw.py` + `draw.sh` + `uid.txt` --> `./img`
-
-- <del>【完整】更新uid.txt中用户的粉丝数、获赞数、播放量、阅读量，需要每月更新SESSDATA：（由于B站似乎更改了SESSDATA的刷新频率，此条弃用）</del>
-  > `general.py` + `general.sh` +`uid.txt` --> `./csv` + `./txt`
-
-
-
-
----
-## 用法
-如果要自己用，可以直接复制后新开repo，也可以Fork之后：
-- 修改 `uid.txt` 里要监控的uid列表
-- 记得修改几个 yml 文件里面 commit 的账号啊！！不然每次都是我在提交
-- [可选]最后根据需要修改 `./.github/workflows/bilibili.yml` 中的监控频率，目前为每天北京时间23:50爬一次
-- <del>修改 `general.py` 中的SESSDATA（完整，已弃用）</del>
-
----
-## 其它
--- draw.py
+++ draw.py
@@ -7,24 +7,30 @@ uid = sys.argv[1]
 # uid = 22245854
 
 df = pd.read_csv('data/%s.csv'%uid)
-df = df.iloc[::-1]
+df = df.iloc[98::-1] # 98意为只截取最近99天的数据，-1意为倒序
 df['diff_follower']=df['follower'].diff(1)
 # print(df.head(16))
 
 plt.figure(figsize=(15,5))
 
 # https://coolors.co/gradient-palette/fcaed6-7a73ff?number=8
-color = pd.Series(['#D4221C', '#D3401C', '#D25E1B', '#D17C1B', '#CF9A1B', '#CEB81A',
-'#CDD61A', '#A9D53C', '#86D55E', '#62D480', '#3FD4A2', '#1BD3C4',
- '#FCAED6', '#E9A6DC', '#D79DE2', '#C495E8','#B28CED', '#9F84F3', '#8D7BF9','#7A73FF',
-  '#8B6ADF', '#9C61BE','#AE589E', '#BF4F7E', '#D0465D','#E13D3D']) # 26个颜色
+color = pd.Series([
+    '#D4221C', '#D3401C', '#D25E1B', '#D17C1B', '#CF9A1B', '#CEB81A',
+    '#CDD61A', '#A9D53C', '#86D55E', '#62D480', '#3FD4A2', '#1BD3C4',
+    '#FCAED6', '#E9A6DC', '#D79DE2', '#C495E8','#B28CED', '#9F84F3', '#8D7BF9','#7A73FF',
+    '#8B6ADF', '#9C61BE','#AE589E', '#BF4F7E', '#D0465D','#E13D3D',
+    '#D4221C', '#D3401C', '#D25E1B', '#D17C1B', '#CF9A1B', '#CEB81A',
+    '#CDD61A', '#A9D53C', '#86D55E', '#62D480', '#3FD4A2', '#1BD3C4',]) # 12+14+12个颜色
 # reverse
 color = color[::-1]
 
-inter = pd.Series([-1000,-500,-200,-100,-50,-20,
--10,-5,-3,-2,-1,0,
-1,2,3,5,10,20,50,100,
-200,500,1000,2000,5000]) # 25个刻度
+inter = pd.Series([
+    -1000,-500,-200,-100,-50,-20,
+    -10,-5,-3,-2,-1,0,
+    1,2,3,4,6,8,11,16,
+    23,32,45,64,90,
+    128,180,256,360,512,720,
+    1024,1440,2048,2880,4096,5760]) # 12+13+12个刻度
 
 # 查找对应颜色
 color_index = inter.searchsorted(df['diff_follower'])
@@ -38,6 +44,8 @@ plt.yscale('symlog',basey=2)
 
 # Y轴标签必须完整展示数字
 plt.gca().yaxis.set_major_formatter(ScalarFormatter())
+# Y轴标签在右边也加一个
+plt.gca().tick_params(labeltop=False, labelright=True)
 # 设置X轴最多有13个ticks
 plt.gca().xaxis.set_major_locator(plt.MaxNLocator(13))
 # 坐标及标签设为白色，网格为灰色
@@ -50,6 +58,8 @@ plt.gca().spines['top'].set_color('none')
 plt.grid(color='gray', linestyle='-', linewidth=0.5)
 # 背景透明并输出
 plt.savefig('img/%s_diff_follower.png'%uid, transparent=True)
+# plt.savefig('test/%s_diff_follower.png'%uid, transparent=True)
 
 plt.yscale('linear')
 plt.savefig('img/%s_diff_follower_ori.png'%uid, transparent=True)
+# plt.savefig('test/%s_diff_follower_ori.png'%uid, transparent=True)
